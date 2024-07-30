d499fb388b97c0c2a2952c783de177b59bdb854e
mw08081 <president304@naver.com>
2024-07-30 19:00:12 +0900

    Added Move,Turn Axis Function Binding && Modified moving mechanism for Caterpillar Mechanism

--- Source/ToonTanks/Tank.cpp
+++ Source/ToonTanks/Tank.cpp
@@ -4,6 +4,7 @@
 #include "Tank.h"
 #include "GameFramework/SpringArmComponent.h"
 #include "Camera/CameraComponent.h"
+#include "Kismet/GameplayStatics.h"
 
 ATank::ATank()
 {
@@ -13,3 +14,63 @@ ATank::ATank()
 	CamComp = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera Comp"));
 	CamComp->SetupAttachment(SpringArm);
 }
+
+
+void ATank::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	//PlayerInputComponent->BindAxis(TEXT("MoveForward"), this, &ATank::Move);
+	//PlayerInputComponent->BindAxis(TEXT("Turn"), this, &ATank::Turn);
+	PlayerInputComponent->BindAxis(TEXT("RightCaterpillar"), this, &ATank::SetRightCaterpillarValByInput);
+	PlayerInputComponent->BindAxis(TEXT("LeftCaterpillar"), this, &ATank::SetLeftCaterpillarValByInput);
+}
+
+void ATank::Move(float value)
+{
+	//UE_LOG(LogTemp, Display, TEXT("%f"), value);
+	forwardVec.X = value * moveSpeed * UGameplayStatics::GetWorldDeltaSeconds(this);
+	AddActorLocalOffset(forwardVec, true);
+}
+
+void ATank::Turn(float value)
+{
+	yawRot.Yaw = value * rotSpeed * UGameplayStatics::GetWorldDeltaSeconds(this);
+	AddActorLocalRotation(yawRot, true);
+}
+
+void ATank::SetRightCaterpillarValByInput(float value)
+{
+	rightCaterpillarVal = value;
+}
+
+void ATank::SetLeftCaterpillarValByInput(float value)
+{
+	leftCaterpillarVal = value;
+}
+
+void ATank::Moving(float dt)
+{
+	if (leftCaterpillarVal == 0 || rightCaterpillarVal == 0) {
+
+	}
+	else {
+		if (leftCaterpillarVal > 0 && rightCaterpillarVal > 0 ||
+			leftCaterpillarVal < 0 && rightCaterpillarVal < 0) {
+			forwardVec.X = leftCaterpillarVal * moveSpeed * dt;
+			AddActorLocalOffset(forwardVec, true);
+		}
+		else {
+			yawRot.Yaw = leftCaterpillarVal * rotSpeed * dt;
+			AddActorLocalRotation(yawRot, true);
+		}
+	}
+}
+
+void ATank::Tick(float DeltaTime)
+{
+	if (leftCaterpillarVal != 0 && rightCaterpillarVal != 0) {
+		Moving(DeltaTime);
+	}
+
+}
+
--- Source/ToonTanks/Tank.h
+++ Source/ToonTanks/Tank.h
@@ -16,14 +16,45 @@ class TOONTANKS_API ATank : public ABasePawn
 
 public :
 	ATank();
+	virtual void Tick(float DeltaTime) override;
 
-	
-
-private :
+//Components
+private:
 	//UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
 	UPROPERTY(VisibleAnywhere)
 		class USpringArmComponent* SpringArm;
-	
+
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
 		class UCameraComponent* CamComp;
+
+//Editor Variable
+public:
+
+private: 
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Player" , meta = (AllowPrivateAccess = "true"))
+		float moveSpeed;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Player" , meta = (AllowPrivateAccess = "true"))
+		float rotSpeed;
+
+//Code Variable
+public:
+
+private:
+	float rightCaterpillarVal = 0.f;
+	float leftCaterpillarVal = 0.f;
+	FVector forwardVec = FVector::ZeroVector;
+	FRotator yawRot = FRotator::ZeroRotator;
+
+//Function
+public :
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	void Move(float value);
+	void Turn(float value);
+	void SetRightCaterpillarValByInput(float value);
+	void SetLeftCaterpillarValByInput(float value);
+	void Moving(float dt);
+
+//private:
 };
